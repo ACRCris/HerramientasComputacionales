a602500232c27eb85b13051fd5b194b8cc87c37c
ACRCris <crperezp@unal.edu.co>
2022-04-19 06:28:51 -0500

    tarea6 to github

--- tareas/tarea7/fixing.cpp
+++ tareas/tarea7/fixing.cpp
@@ -8,28 +8,34 @@ double baz(double x);
 
 int main (int argc, char **argv)
 {
-int ii, jj;
-  ii =  0;
+  int ii, jj;
+  ii = 0;
   jj = -1;
-        foo(ii, jj);
+  foo(ii, jj);
   foo(jj, ii);
-baz(25.9);
+  baz(25.9);
 
   return EXIT_SUCCESS;
 }
 
-int foo(int a, int b){ return a/b + b/bar(a, b) + b/a;
+int foo(int a, int b){
+  if(a == 0 || b==0){
+    if(bar(a,b) != 0)
+      return b/bar(a,b);
+    else return 0;
+  }else
+    return  a/b + b/bar(a, b) + b/a;
 }
 
-   int bar(int a, int b)
+int bar(int a, int b)
 {
-         unsigned int c = a;
-return c + a - b;
+  unsigned int c = a;
+  return c + a - b;
 }
 
 double baz(double x)
 {
-if (x = 0) return;
-double v = 1-(x+1);
-return std::sqrt(x)
+  if (x == 0) return 0;
+  double v = 1-(x+1);
+  return std::sqrt(v);
 }
